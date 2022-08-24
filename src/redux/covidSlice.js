ca86f4cf53c78b70523b48aab76396efc7c6fbb6
Cem <cmbicakci@gmail.com>
2022-08-24 20:35:33 +0300

    added axios and asyncThunk

--- src/redux/covidSlice.js
+++ src/redux/covidSlice.js
@@ -1,9 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchData = createAsyncThunk('covidData/fetchData', async () => {
+    const { data } = await axios('https://covid19.mathdro.id/api');
+})
 
 export const covidSlice = createSlice({
     name: 'covid',
     initialState: {
-        items:{}
+        items: {}
     }
 })
 
