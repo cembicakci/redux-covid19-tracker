d380f2606e872f8ffc0c354e0ea452e25c98f66e
Cem <cmbicakci@gmail.com>
2022-08-24 20:18:39 +0300

    added Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <img src='https://world19covid.web.app/static/media/image.d7265326.png'></img>
+        <p><strong>Global and Country Wise Cases of Corona Virus</strong></p>
+        <p><i>(For a Particular country, select a Country from below)</i></p>
+    </header>
+  )
+}
+
+export default Header
