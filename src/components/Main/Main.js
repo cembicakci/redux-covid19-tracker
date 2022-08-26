c2c504982df0e89af0320930912227705acf17a5
Cem <cmbicakci@gmail.com>
2022-08-26 20:42:43 +0300

    added Cards

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,7 @@ function Main() {
     const countries = useSelector(state => state.covid.countries);
     const globalData = useSelector(state => state.covid.globalData);
 
-    const [selectedCountry, setSelectedCountry] = useState()
+    const [selectedCountry, setSelectedCountry] = useState('');
 
 
     console.log("countries", countries)
@@ -19,24 +19,65 @@ function Main() {
     useEffect(() => {
         dispatch(fetchData())
         dispatch(fetchDataCountries())
-    }, [dispatch])
+    }, [])
 
 
     function handleCountries(e) {
         setSelectedCountry(e.target.value);
     }
-    
+
 
     return (
-        <div>
-            <select value={selectedCountry} onChange={handleCountries}>
-                {countries &&
-                    countries.countries.map((country, index) => (
-                        <option value={country.name} key={index}>{country.name}</option>
-                    ))
-                }
+        <div className='main'>
+            {
+                globalData &&
+                <div className='main-details'>
+                    <div className='main-box infected'>
+                        <h3>Infected</h3>
+                        <p className='main-box-value'>{globalData.confirmed.value}</p>
+                        <p>Last Updated at :</p>
+                        <span>{ new Date(globalData.lastUpdate).toLocaleString()}</span>
+                        <p className='main-box-desc'>Number of infect cases of COVID-19</p>
+                    </div>
 
-            </select>
+                    <div className='main-box recovered'>
+                        <h3>Recovered</h3>
+                        <p className='main-box-value'>{globalData.recovered.value}</p>
+                        <p>Last Updated at :</p>
+                        <span>{ new Date(globalData.lastUpdate).toLocaleString()}</span>
+                        <p className='main-box-desc'>Number of recoveries from COVID-19</p>
+                    </div>
+
+                    <div className='main-box death'>
+                        <h3>Deaths</h3>
+                        <p className='main-box-value'>{globalData.deaths.value}</p>
+                        <p>Last Updated at :</p>
+                        <span>{ new Date(globalData.lastUpdate).toLocaleString()}</span>
+                        <p className='main-box-desc'>Number of deaths caused by COVID-19</p>
+                    </div>
+
+                    <div className='main-box active'>
+                        <h3>Active</h3>
+                        <p className='main-box-value'>{globalData.confirmed.value - globalData.deaths.value}</p>
+                        <p>Last Updated at :</p>
+                        <span>{ new Date(globalData.lastUpdate).toLocaleString()}</span>
+                        <p className='main-box-desc'>Number of active cases of COVID-19</p>
+                    </div>
+
+                </div>
+            }
+
+            <div>
+                <select value={selectedCountry} onChange={handleCountries}>
+                    <option>Global</option>
+                    {countries &&
+                        countries.countries.map((country, index) => (
+                            <option value={country.name} key={index}>{country.name}</option>
+                        ))
+                    }
+
+                </select>
+            </div>
         </div>
     )
 }
