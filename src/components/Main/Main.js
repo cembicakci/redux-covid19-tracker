7f4ccf1e001540e020ca137fe3c5b0ee9e72c60c
Cem <cmbicakci@gmail.com>
2022-08-26 22:05:11 +0300

    added selected country api

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { fetchData, fetchDataCountries } from '../../redux/covidSlice'
+import { setSelectedCountry } from '../../redux/covidSlice';
 import { useDispatch, useSelector } from 'react-redux'
 import CountUp from "react-countup";
 
@@ -7,40 +8,35 @@ function Main() {
 
     const dispatch = useDispatch();
     const countries = useSelector(state => state.covid.countries);
-    const globalData = useSelector(state => state.covid.globalData);
-
-    const [selectedCountry, setSelectedCountry] = useState('');
-
-
-    console.log("countries", countries)
-    console.log("globalData", globalData)
-
-
+    const covidData = useSelector(state => state.covid.covidData);
+    const selectedCountry = useSelector(state => state.covid.selectedCountry);
 
     useEffect(() => {
-        dispatch(fetchData())
+        dispatch(fetchData(selectedCountry))
         dispatch(fetchDataCountries())
     }, [])
 
 
     function handleCountries(e) {
-        setSelectedCountry(e.target.value);
+        dispatch(setSelectedCountry(e.target.value))
+        dispatch(fetchData(e.target.value))
+        
     }
 
 
     return (
         <div className='main'>
             {
-                globalData &&
+                covidData &&
                 <div className='main-details'>
                     <div className='main-box infected'>
                         <h3>Infected</h3>
                         <p className='main-box-value'>
-                            <CountUp start={0} end={globalData.confirmed.value} duration={1} separator="," className='text-3xl ld:text-5xl py-10' />
+                            <CountUp start={0} end={covidData.confirmed.value} duration={1} separator="," className='text-3xl ld:text-5xl py-10' />
                         </p>
                         <div>
                             <p>Last Updated at :</p>
-                            <p className='main-box-date'>{new Date(globalData.lastUpdate).toLocaleString()}</p>
+                            <p className='main-box-date'>{new Date(covidData.lastUpdate).toLocaleString()}</p>
                             <p className='main-box-desc'>Number of infect cases of COVID-19</p>
                         </div>
                     </div>
@@ -48,11 +44,11 @@ function Main() {
                     <div className='main-box recovered'>
                         <h3>Recovered</h3>
                         <p className='main-box-value'>
-                            <CountUp start={0} end={globalData.recovered.value} duration={1} separator="," className='text-3xl ld:text-5xl py-10' />
+                            <CountUp start={0} end={covidData.recovered.value} duration={1} separator="," className='text-3xl ld:text-5xl py-10' />
                         </p>
                         <div>
                             <p>Last Updated at :</p>
-                            <p className='main-box-date'>{new Date(globalData.lastUpdate).toLocaleString()}</p>
+                            <p className='main-box-date'>{new Date(covidData.lastUpdate).toLocaleString()}</p>
                             <p className='main-box-desc'>Number of recoveries from COVID-19</p>
                         </div>
                     </div>
@@ -60,11 +56,11 @@ function Main() {
                     <div className='main-box death'>
                         <h3>Deaths</h3>
                         <p className='main-box-value'>
-                            <CountUp start={0} end={globalData.deaths.value} duration={1} separator="," className='text-3xl ld:text-5xl py-10' />
+                            <CountUp start={0} end={covidData.deaths.value} duration={1} separator="," className='text-3xl ld:text-5xl py-10' />
                         </p>
                         <div>
                             <p>Last Updated at :</p>
-                            <p className='main-box-date'>{new Date(globalData.lastUpdate).toLocaleString()}</p>
+                            <p className='main-box-date'>{new Date(covidData.lastUpdate).toLocaleString()}</p>
                             <p className='main-box-desc'>Number of deaths caused by COVID-19</p>
                         </div>
                     </div>
@@ -72,11 +68,11 @@ function Main() {
                     <div className='main-box active'>
                         <h3>Active</h3>
                         <p className='main-box-value'>
-                            <CountUp start={0} end={globalData.confirmed.value - globalData.deaths.value} duration={1} separator="," className='text-3xl ld:text-5xl py-10' />
+                            <CountUp start={0} end={covidData.confirmed.value - covidData.deaths.value} duration={1} separator="," className='text-3xl ld:text-5xl py-10' />
                         </p>
                         <div>
                             <p>Last Updated at :</p>
-                            <p className='main-box-date'>{new Date(globalData.lastUpdate).toLocaleString()}</p>
+                            <p className='main-box-date'>{new Date(covidData.lastUpdate).toLocaleString()}</p>
                             <p className='main-box-desc'>Number of active cases of COVID-19</p>
                         </div>
                     </div>
--- src/redux/covidSlice.js
+++ src/redux/covidSlice.js
@@ -1,9 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchData = createAsyncThunk('data/fetchData', async () => {
-    const res = await axios.get('https://covid19.mathdro.id/api');
-    return res.data
+export const fetchData = createAsyncThunk('data/fetchData', async (selectedCountry) => {
+    if (selectedCountry === '') {
+        const res = await axios.get('https://covid19.mathdro.id/api');
+        return res.data
+    } else {
+        const res = await axios(`https://covid19.mathdro.id/api/countries/${selectedCountry}`);
+        console.log(res.data)
+        return res.data
+    }
 })
 
 export const fetchDataCountries = createAsyncThunk('data/fetchDataCountries', async () => {
@@ -14,12 +20,18 @@ export const fetchDataCountries = createAsyncThunk('data/fetchDataCountries', as
 export const covidSlice = createSlice({
     name: 'covid',
     initialState: {
-        globalData: '',
-        countries: ''
+        covidData: '',
+        countries: '',
+        selectedCountry: ''
+    },
+    reducers: {
+        setSelectedCountry: (state, action) => {
+            state.selectedCountry = action.payload;
+        }
     },
     extraReducers: {
         [fetchData.fulfilled]: (state, action) => {
-            state.globalData = action.payload;
+            state.covidData = action.payload;
         },
         [fetchDataCountries.fulfilled]: (state, action) => {
             state.countries = action.payload;
@@ -27,4 +39,5 @@ export const covidSlice = createSlice({
     }
 })
 
+export const { setSelectedCountry } = covidSlice.actions;
 export default covidSlice.reducer;
