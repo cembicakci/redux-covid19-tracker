014e7161b327203c733af7e28002561367d6353d
Cem <cmbicakci@gmail.com>
2022-08-26 19:59:21 +0300

    added dropdown

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { fetchData, fetchDataCountries } from '../../redux/covidSlice'
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -8,20 +8,35 @@ function Main() {
     const countries = useSelector(state => state.covid.countries);
     const globalData = useSelector(state => state.covid.globalData);
 
+    const [selectedCountry, setSelectedCountry] = useState()
+
+
     console.log("countries", countries)
     console.log("globalData", globalData)
+
 
 
     useEffect(() => {
         dispatch(fetchData())
         dispatch(fetchDataCountries())
-    }, [])
+    }, [dispatch])
 
+
+    function handleCountries(e) {
+        setSelectedCountry(e.target.value);
+    }
+    
 
     return (
         <div>
-            {countries &&
-                countries.countries.map((country) => <p>{country.name}</p>)}
+            <select value={selectedCountry} onChange={handleCountries}>
+                {countries &&
+                    countries.countries.map((country, index) => (
+                        <option value={country.name} key={index}>{country.name}</option>
+                    ))
+                }
+
+            </select>
         </div>
     )
 }
