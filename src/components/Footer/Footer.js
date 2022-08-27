2d3358d1310dadc032cbd8cad24a71684f327bad
Cem <cmbicakci@gmail.com>
2022-08-27 19:57:50 +0300

    added Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <h2>Created with care by <a href='https://www.github.com/cembicakci' target='_blank'>Cem Bıçakcı</a></h2>
+
+        </footer>
+    )
+}
+
+export default Footer
